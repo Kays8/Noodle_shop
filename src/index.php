04f9f919d47c65ca34c75a791bb94f48fce7c314
Kays8 <thirayut1379@gmail.com>
2023-03-24 00:16:08 +0700

    update

--- src/index.php
+++ src/index.php
@@ -1,52 +1,65 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
-        <title>CRUD Customer Information</title>
-    </head>
-    <body>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12"> <br>
-                    <h3>Noodle Shop <a href="addCustomer_dropdown.php" class="btn btn-info float-end">+เพิ่มข้อมูล</a> </h3>
-                    <table class="table table-striped  table-hover table-responsive table-bordered">
-                        <thead align="center">                        
-                            <tr>
-                                <th width="20%">จำนวน</th>
-                                <th width="10%">ชื่ออาหาร</th>
-                                <th width="20%">ราคา</th>
-                                
-                            </tr>                       
-                        </thead>
 
-                        <tbody>
-                          <?php
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <title>CRUD Customer Information</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12"> <br>
+                <h3>Noodle Shop <a href="addFood_dropdown.php" class="btn btn-info float-end">+เพิ่มข้อมูล</a> </h3>
+                <table class="table table-striped  table-hover table-responsive table-bordered">
+                    <thead align="center">
+                        <tr>
+                            <th width="10%">รหัสอาหาร</th>
+                            <th width="25%">รายการอาหาร</th>
+                            <th width="20%">ราคา</th>
+                            <th width="20%">ประเภท</th>
+                            <th width="50">รูปภาพ</th>
+                            <th width="5%">แก้ไข</th>
+                            <th width="5%">ลบ</th>
+
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <?php
                             require '../connect.php';
-                            $sql =  "SELECT * FROM food,order_detail WHERE food.FoodID AND order_detail.Quantity";
+                            $sql =  "SELECT * FROM food,menu WHERE food.FoodID = menu.FoodTypeID";
                             $stmt = $conn->prepare($sql);
                             $stmt->execute();
                             $result = $stmt->fetchAll();
                             foreach ($result as $r) { ?>
-                            <tr>
-                                <td><?= $r['Quantity'] ?></td>
-                                <td><?= $r['FoodName'] ?></td>
-                                <td><?= $r['FoodPrice'] ?></td>
-                                
-                                
-                    
-                                <td><a href="updateCustomerForm.php?CustomerID=<?= $r['CustomerID'] ?>" class="btn btn-warning btn-sm">แก้ไข</a></td>
-                                <td><a href="deleteCustomer.php?CustomerID=<?= $r['CustomerID'] ?>" class="btn btn-danger btn-sm" onclick="return confirm('ยืนยันการลบข้อมูล !!');">ลบ</a></td>
-                            </tr>
-                          <?php 
+                        <tr>
+                            <td><?= $r['FoodTypeID'] ?></td>
+                            <td><?= $r['FoodName'] ?></td>
+                            <td><?= $r['FoodPrice'] ?></td>
+                            <td><?= $r['FoodTypeName'] ?></td>
+                            <td><img src="../images/D1.jpg" width="50px" height="50" alt="image" ></td>
+            
+
+
+                            <td><a href="addFoodForm.php?FoodID=<?= $r['FoodID'] ?>"
+                                    class="btn btn-warning btn-sm">แก้ไข</a></td>
+
+                            <td><a href="deleteFood.php?FoodID=<?= $r['FoodID'] ?>" class="btn btn-danger btn-sm"
+                                    onclick="return confirm('ยืนยันการลบข้อมูล !!');">ลบ</a></td>
+                        </tr>
+                        <?php 
                            }
                           ?>
-                        </tbody>
-                    </table>
-                </div>
+                    </tbody>
+                </table>
             </div>
-        </div>   
-    </body>
+        </div>
+    </div>
+</body>
+
 </html>
